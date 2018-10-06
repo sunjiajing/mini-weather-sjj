85bd4b32a90910a72f0dcb970fb153dce5d44903
sunjiajing <836027084@qq.com>
2018-10-06 17:07:48 +0800

    顶部工具栏制作

---
+++ app/src/main/java/cn/edu/pku/sunjiajing/miniweather/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.pku.sunjiajing.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+
+}
