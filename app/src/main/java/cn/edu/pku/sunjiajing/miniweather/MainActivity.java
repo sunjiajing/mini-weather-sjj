b3ac6391355bdbea6afddcaf0c126b48d11a9c22
sunjiajing <836027084@qq.com>
2018-10-09 20:23:15 +0800

    网络状态检查

--- app/src/main/java/cn/edu/pku/sunjiajing/miniweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/sunjiajing/miniweather/MainActivity.java
@@ -2,6 +2,10 @@ package cn.edu.pku.sunjiajing.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.edu.pku.sunjiajing.util.NetUtil;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +13,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+
+        if(NetUtil.getNetworkState(this) != NetUtil.NETWOR_NONE){
+            Log.d("myWeather","网络OK");
+            Toast.makeText(MainActivity.this,"网络OK!",Toast.LENGTH_LONG).show();
+        }else{
+            Log.d("myWeather","网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
+        }
     }
 
 }
